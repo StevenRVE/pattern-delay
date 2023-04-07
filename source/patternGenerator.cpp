6aec2727f91a413cde184a63db95eb9b6492452d
Steven van Esch <steven_ve@live.nl>
2023-04-07 15:10:18 +0200

    Added patternGenerator class

---
+++ source/patternGenerator.cpp
@@ -0,0 +1,34 @@
+//
+// Created by StevenRVE on 7-4-2023.
+//
+
+#include "patternGenerator.hpp"
+
+PatternGenerator::PatternGenerator() :
+    patternNumber(0)
+{
+
+}
+
+PatternGenerator::~PatternGenerator()
+{
+
+}
+
+void PatternGenerator::generatePattern()
+{
+
+}
+
+void PatternGenerator::setPattern(uint8_t newPatternNumber)
+{
+    if(newPatternNumber>=0 && newPatternNumber<PATTERN_TYPE_COUNT)
+    {
+        this->patternNumber=newPatternNumber;
+    }
+}
+
+uint8_t PatternGenerator::getPatternNumber() const
+{
+    return patternNumber;
+}
---
+++ source/patternGenerator.hpp
@@ -0,0 +1,36 @@
+//
+// Created by StevenRVE on 7-4-2023.
+//
+
+#ifndef SVE_PATTERN_DELAY_PATTERNGENERATOR_HPP
+#define SVE_PATTERN_DELAY_PATTERNGENERATOR_HPP
+
+#include <cstdint>
+#include <array>
+
+class PatternGenerator {
+public:
+    enum PatternType{
+        PATTERN_TYPE_NTH,
+        PATTERN_TYPE_EUCLIDEAN,
+        PATTERN_TYPE_RANDOM,
+        PATTERN_TYPE_COUNT
+    };
+
+    PatternGenerator();
+
+    ~PatternGenerator();
+
+    void generatePattern();
+    void setPattern(uint8_t newPatternNumber);
+    uint8_t getPatternNumber() const;
+
+private:
+    PatternType patternType;
+    uint8_t patternNumber;
+
+    std::array<bool,8> pattern { 0 };
+};
+
+
+#endif //SVE_PATTERN_DELAY_PATTERNGENERATOR_HPP
